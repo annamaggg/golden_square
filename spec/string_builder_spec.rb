fe385e32e6f02d10e63fa75b1bb866845aa98281
annamaggg <annamagnusson99@gmail.com>
2023-01-16 15:45:11 +0000

    stringbuilder

---
+++ spec/string_builder_spec.rb
@@ -0,0 +1,21 @@
+require 'string_builder'
+
+RSpec.describe StringBuilder do
+  it "returns the length of a string" do
+    string = StringBuilder.new()
+    string.add("hello")
+
+    length = string.size
+    expect(length).to eq 5
+ 
+  end
+  
+  it "returns the string" do
+    string = StringBuilder.new()
+    string.add("hello")
+
+    text = string.output
+    expect(text).to eq "hello"
+  end
+end
+    
