80420894f4911427c214f3400f689841fb847810
annamaggg <annamagnusson99@gmail.com>
2023-01-18 20:34:28 +0000

    exercise2 complete (debugging)

--- debugging/exercise2.rb
+++ debugging/exercise2.rb
@@ -1,9 +1,9 @@
 def encode(plaintext, key)
   cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
   ciphertext_chars = plaintext.chars.map do |char|
-    (65 + cipher.find_index(char)).chr
+    (65 + cipher.find_index(char).to_i).chr #######to_i
   end
-  return ciphertext_chars.join
+  puts ciphertext_chars.join
 end
 
 def decode(ciphertext, key)
@@ -11,13 +11,13 @@ def decode(ciphertext, key)
   plaintext_chars = ciphertext.chars.map do |char|
     cipher[65 - char.ord]
   end
-  return plaintext_chars.join
+  puts plaintext_chars.join
 end
 
 # Intended output:
 #
-# > encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+ encode("theswiftfoxjumpedoverthelazydog", "secretkey")
 # => "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
 #
-# > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+ decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
 # => "theswiftfoxjumpedoverthelazydog"
